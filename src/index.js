5fafc54f513e68d34aa27216a957d641da0ceee1
MayankRapidops <mayank.h.patel@rapidops.co>
2024-12-06 00:00:50 +0530

    Added index file server code

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors'); // Import the cors package
+const app = express();
+const port = 8083;
+
+// Import the routes from the router directory
+const router = require('./routes');
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Parse incoming JSON requests
+app.use(express.json());
+
+// Use the routes for all paths
+app.use('/', router);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
